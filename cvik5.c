9fc03e4f9c6320d12ce05801643b8c736d30a7f6
selfdestoy <zanejrode@gmail.com>
2025-10-14 23:51:19 +0300

    asd

--- cvik5.c
+++ cvik5.c
@@ -1,3 +1,26 @@
-//
-// Created by user on 14.10.2025.
-//
+#include <stdio.h>
+
+int main(void) {
+    struct date_t{
+        int year;
+        int month;
+        int day;
+    };
+}
+    int valid(struct date_t d) {
+
+        if (d.year < 0 || d.month < 1 || d.month > 12 || d.day < 1) {
+            return 0;
+        }
+        int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    //prestupny rok
+        if (d.year % 4 == 0 && d.year % 100 != 0 || d.year % 400 == 0) {
+            days[1] = 29;
+        }
+
+
+
+
+    return 1;
+}
