3b69f7ed264b4d7c6b7165a26770033b5c58fa61
selfdestoy <zanejrode@gmail.com>
2025-10-17 23:38:49 +0200

    FINAL

--- keyfilter.c
+++ keyfilter.c
@@ -3,40 +3,18 @@
 #include <string.h>
 //magnum opus
 
-void print_result(char enable[256], char matches[256], int counter, int found, char *prefix) {
-    if (counter == 0) { //nenaslo se zadne match
-        printf("Not found");
-    }
-    if ((found == 1 && counter == 1) || strlen(prefix) == strlen(matches)){ //druha podminka je spis pro york a yorktown
-        printf("Found: ");
-        for (int i = 0; i < (int)strlen(matches); i++) {
-            printf("%c", toupper(matches[i]));
-        }
-        printf("\n");
-    }
-    if (enable[0] != '\0' && counter > 1) {
-        printf("Enable: %s\n", enable);
-    }
-}
+void print_result(char enable[256], char matches[256], int counter, int found, char *prefix);
 
-char* qsort(char enable[], int len) {
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (enable[i] > enable[j]) { //pokud symboly nejdou po sobe (treba za abecedou)
-                char qsort = enable[i]; //dame je jedno za druhym
-                enable[i] = enable[j];
-                enable[j] = qsort;
-            }
-        }
-    }
-    return enable;
-}
+char* qsort(char enable[], int len);
+
+void inicializace_enable(char enable[256], char next_letter, int exist, char lines[101], char *prefix);
+//popis fuknci viz pri pouziti
 
 int main(int argc, char *argv[])
 {
     char lines[101]; char enable[256] = {0}; char matches[256] = {0};
     int i = 0; int found = 0; int counter = 0; int match = 0; int longer = 0;
-    char *prefix; char next_char = '\0';
+    char *prefix; char next_letter = '\0';
 
     if (argc < 2) { //pokud je prefix prazdni
         prefix = "";
@@ -67,31 +45,64 @@ int main(int argc, char *argv[])
                 strcpy(matches, lines); //pokud mame jenom jeden radek, ktery ma v sobe prefix - ulozime jej
             }
             if (counter > 1 || (strlen(lines) > strlen(prefix))) {
-                next_char = toupper(lines[strlen(prefix)]); // pokud mame vice variant, pridame dalsi(po prefixu) symbol pro hledani
+                next_letter = toupper(lines[strlen(prefix)]); // pokud mame vice variant, pridame dalsi(po prefixu) symbol pro hledani
             }
             if (strlen(prefix) == 0) { //prazdny prefix
-                next_char = toupper(lines[0]); //vypise prvni slopec
+                next_letter = toupper(lines[0]); //vypise prvni slopec
             }else {
                 if (!longer || strlen(prefix) == strlen(lines)) {
                     found = 1; //pokud
                 }
             }
             int exist = 0;
-            for (int j = 0; enable[j] != '\0'; j++) {
-                if (next_char == enable[j]) { //pokud jiz mame symbol v enable - nic se nezmeni
-                    exist = 1;
-                    break;
-                }
-            }
-            if (!exist && next_char != '\n' && next_char != '\0' && strlen(lines) > strlen(prefix)) {
-                int len = strlen(enable);
-                enable[len] = next_char; //pokud znak se nenachazi jeste v enavble - pridame jej na konec
-                enable[len + 1] = '\0';
-                len = strlen(enable); //po tom, co jsme pridali do enable, obnovime len, protoze ono se uz nerovna tomu, co jsme meli na zacatku
-                qsort(enable, len); //nahrada misto qsort))
-            }
+            inicializace_enable(enable, next_letter, exist, lines, prefix); //do pole enable predavame symboly, co jsme dostali po kontrolach, a odstranujeme stejna pismena
         }
     }
     print_result(enable, matches, counter, found, prefix); //funkce pro print vysledku
     return 0;
+}
+
+char* qsort(char enable[], int len) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (enable[i] > enable[j]) { //pokud symboly nejdou po sobe (treba za abecedou)
+                char qsort = enable[i]; //dame je jedno za druhym
+                enable[i] = enable[j];
+                enable[j] = qsort;
+            }
+        }
+    }
+    return enable;
+}
+
+void print_result(char enable[256], char matches[256], int counter, int found, char *prefix) {
+    if (counter == 0) { //nenaslo se zadne match
+        printf("Not found");
+    }
+    if ((found == 1 && counter == 1) || strlen(prefix) == strlen(matches)){ //druha podminka je spis pro york a yorktown
+        printf("Found: ");
+        for (int i = 0; i < (int)strlen(matches); i++) {
+            printf("%c", toupper(matches[i]));
+        }
+        printf("\n");
+    }
+    if (enable[0] != '\0' && counter > 1) {
+        printf("Enable: %s\n", enable);
+    }
+}
+
+void inicializace_enable(char enable[256], char next_letter, int exist, char lines[101], char *prefix) {
+    for (int j = 0; enable[j] != '\0'; j++) {
+        if (next_letter == enable[j]) { //pokud jiz mame symbol v enable - nic se nezmeni
+            exist = 1;
+            break;
+        }
+    }
+    if (!exist && next_letter != '\n' && next_letter != '\0' && strlen(lines) > strlen(prefix)) {
+        int len = strlen(enable);
+        enable[len] = next_letter; //pokud znak se nenachazi jeste v enavble - pridame jej na konec
+        enable[len + 1] = '\0';
+        len = strlen(enable); //po tom, co jsme pridali do enable, obnovime len, protoze ono se uz nerovna tomu, co jsme meli na zacatku
+        qsort(enable, len); //nahrada misto qsort))
+    }
 }
