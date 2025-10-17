84c10e03e35ee75209a0acffbd0b08dbb9e0b976
selfdestoy <zanejrode@gmail.com>
2025-10-17 23:35:57 +0300

    skoro

--- keyfilter.c
+++ keyfilter.c
@@ -1,13 +1,13 @@
-#include <ctype.h>
+#include  <ctype.h>
 #include <stdio.h>
 #include <string.h>
-// magnum opus
+//magnum opus
 
 void print_result(char enable[256], char matches[256], int counter, int found, char *prefix) {
-    if (counter == 0) {
+    if (counter == 0) { //nenaslo se zadne match
         printf("Not found");
     }
-    if ((found == 1 && counter == 1) || (strncmp(matches, prefix, strlen(prefix)) == 0 && strlen(prefix) == strlen(matches))) {
+    if ((found == 1 && counter == 1) || strlen(prefix) == strlen(matches)){ //druha podminka je spis pro york a yorktown
         printf("Found: ");
         for (int i = 0; i < (int)strlen(matches); i++) {
             printf("%c", toupper(matches[i]));
@@ -21,11 +21,11 @@ void print_result(char enable[256], char matches[256], int counter, int found, c
 
 char* qsort(char enable[], int len) {
     for (int i = 0; i < len; i++) {
-        for ( int j = i + 1; j < len; j++ ) {
-            if (enable[i] > enable[j]) { // nahrada misto qsort :)
-                char qsort = enable[i];
+        for (int j = i + 1; j < len; j++) {
+            if (enable[i] > enable[j]) { //pokud symboly nejdou po sobe (treba za abecedou)
+                char qsort = enable[i]; //dame je jedno za druhym
                 enable[i] = enable[j];
-                enable[j] = qsort; // pokud jakykoli symbol je mensi dalsiho, zmenime
+                enable[j] = qsort;
             }
         }
     }
@@ -38,78 +38,60 @@ int main(int argc, char *argv[])
     int i = 0; int found = 0; int counter = 0; int match = 0; int longer = 0;
     char *prefix; char next_char = '\0';
 
-    if (argc < 2) { // na pripad kdyz prefix prazdny
+    if (argc < 2) { //pokud je prefix prazdni
         prefix = "";
-    } else {
+    }else {
         prefix = argv[1];
-        for (int q = 0; argv[1][q] != '\0'; q++) { // case_insensitive pro prefix
-            argv[1][q] = toupper(argv[1][q]);
+        for (int q = 0; argv[1][q] != '\0'; q++) {
+            argv[1][q] = toupper(argv[1][q]); //case_insensetive pro prefix
         }
     }
-    while (fgets(lines, sizeof(lines), stdin) != NULL) {
-        // cteme cely vstup
-        if (lines[strlen(lines) - 1] == '\n') { // pokud radek z stdin konci \n - odstanime
-            lines[strlen(lines) - 1] = '\0';
+    while (fgets(lines, sizeof(lines), stdin) != NULL) { //cteme cely vstup
+        if (lines[strlen(lines) - 1] == '\n') { //pokud znak konci na \n, coz ma vliv na porovnani
+            lines[strlen(lines) - 1] = '\0'; //dame na konec symbol konce radku(prazdny char)
         }
         match = 1;
         for (i = 0; prefix[i] != '\0'; i++) {
-            lines[i] = toupper(lines[i]); // case_insensetive
-            if (prefix[i] != lines[i]) {  // kontrola prefixu a vstupu na stejnem radku
+            lines[i] = toupper(lines[i]); //case_insensitive pro sloupce
+            if (prefix[i] != lines[i]) {
                 match = 0;
                 break;
             }
         }
         if (match == 1) {
-            counter++;
+            counter++; //counter, kolik mame radku shodujici c prefixem
             if (counter > 1 && strlen(lines) > strlen(prefix)) {
-                longer = 1;
+                longer = 1; //vice variant a prefix je mensi za radek
             }
             if (counter == 1) {
-                strcpy(matches, lines);
+                strcpy(matches, lines); //pokud mame jenom jeden radek, ktery ma v sobe prefix - ulozime jej
             }
             if (counter > 1 || (strlen(lines) > strlen(prefix))) {
-                next_char = toupper(lines[strlen(prefix)]);
-                next_char = toupper(next_char);
+                next_char = toupper(lines[strlen(prefix)]); // pokud mame vice variant, pridame dalsi(po prefixu) symbol pro hledani
             }
-            if (strlen(prefix) == 0) {
-                next_char = toupper(lines[0]); // pokud arhument neni zadan - vypise se prvni slopec
-            } else {
-                if ((strncmp(lines, prefix, strlen(prefix)) == 0 && !longer) || strlen(prefix) == strlen(lines)) {
-                    found = 1;
+            if (strlen(prefix) == 0) { //prazdny prefix
+                next_char = toupper(lines[0]); //vypise prvni slopec
+            }else {
+                if (!longer || strlen(prefix) == strlen(lines)) {
+                    found = 1; //pokud
                 }
-                // pokud pristi symbol je odlisni od vsech, co obsahuje prefix, tak pridame dalsi symbol ze vstupu
-                /* udelam zde poznamku pro sebe na priste
-                 * napr. pokud prefix = "ab", teda ma v sobe dva symboly, tak strlen(prefix) = 2;
-                 * zaroven s tim lines = "abc", z toho vychazi ze lines[strlen(prefix)] = lines[2] = 'c', tento znak
-                 * predavame promenne, ktera obsahuje dalsi mozny znak;
-                 */
-                next_char = toupper(next_char); // case_insensitive
             }
             int exist = 0;
-            for (int j = 0; enable[j] != '\0'; j++) { // kontrolujeme unikatnost do konce radku
-                if (next_char == enable[j]) {
-                    exist = 1; // pokud symbol z next_char jiz jsme potkali, tak opravdu existuje
+            for (int j = 0; enable[j] != '\0'; j++) {
+                if (next_char == enable[j]) { //pokud jiz mame symbol v enable - nic se nezmeni
+                    exist = 1;
                     break;
                 }
             }
             if (!exist && next_char != '\n' && next_char != '\0' && strlen(lines) > strlen(prefix)) {
                 int len = strlen(enable);
-                enable[len] = next_char; // pokud program jeste symbol ne potkal, tak pridame jej na konec radku moznych symbolu
-                enable[len + 1] = '\0'; // pridame konec
-                len = strlen(enable);
-                qsort(enable, len); //vlastna funkce
-               /* for (int i = 0; i < len; i++) {
-                    for ( int j = i + 1; j < len; j++ ) {
-                    if (enable[i] > enable[j]) { // nahrada misto qsort :)
-                        char qsort = enable[i];
-                        enable[i] = enable[j];
-                        enable[j] = qsort; // pokud jakykoli symbol je mensi dalsiho, zmenime
-                    }
-                    
-                }*/
+                enable[len] = next_char; //pokud znak se nenachazi jeste v enavble - pridame jej na konec
+                enable[len + 1] = '\0';
+                len = strlen(enable); //po tom, co jsme pridali do enable, obnovime len, protoze ono se uz nerovna tomu, co jsme meli na zacatku
+                qsort(enable, len); //nahrada misto qsort))
             }
         }
     }
-    print_result(enable, matches, counter, found, prefix);
+    print_result(enable, matches, counter, found, prefix); //funkce pro print vysledku
     return 0;
 }
